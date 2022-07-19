8e0cc3b621b6fd6e65b20e86885aa644cf4f3110
oluwabamidele Ayanda <67349146+oayanda@users.noreply.github.com>
2022-07-19 10:27:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+answer = random.randint(0,1)
+if answer == 1:
+  print("Heads")
+else:
+  print("Tails")
+
+
+
+
+
+
+
+
+
